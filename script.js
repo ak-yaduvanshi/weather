4c263ae6338c87b64e84bc59a0154f3a37d7fac1
ak-yaduvanshi <155431309+ak-yaduvanshi@users.noreply.github.com>
2024-02-10 14:32:37 +0530

    up

--- script.js
+++ script.js
@@ -2,12 +2,9 @@
 
 const dayH = document.querySelector(".default_day");
 const dateH = document.querySelector(".default_date");
-const btn = document.querySelector(".btn_search");
 const inputBox = document.querySelector(".input-box");
 
-const icons = document.querySelector(".icons");
 const dayInfo = document.querySelector(".day_info");
-const listContent = document.querySelector(".list_content ul");
 const temperature = document.querySelector(".weather_temp");
 const description = document.querySelector(".description");
 const nameCity = document.querySelector(".name");
@@ -33,26 +30,40 @@ const day3 = document.querySelector(".day3");
 const day4 = document.querySelector(".day4");
 
 const list = document.querySelector(".list_content");
+const times = document.querySelector(".time");
+const content = document.querySelectorAll(".content");
+console.log(content);
 
 const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
 ];
 
 const dt = new Date();
+
 const dayName = days[dt.getDay()];
 // console.log(dayName);
 dayH.innerHTML = dayName;
 
-const dayNext1 = days[dt.getDay() + 1].slice(0,3);
-const dayNext2 = days[dt.getDay() + 2].slice(0,3);
-const dayNext3 = days[dt.getDay() + 3].slice(0,3);
-const dayNext4 = days[dt.getDay() + 4].slice(0,3);
+
+let dayNext1 = days[new Date(dt.getFullYear(), dt.getMonth(), dt.getDate() + 1).getDay()].slice(0, 3);
+// console.log(dayNext1);
+
+let dayNext2 = days[new Date(dt.getFullYear(), dt.getMonth(), dt.getDate() + 2).getDay()].slice(0, 3);
+// console.log(dayNext2);
+
+let dayNext3 = days[new Date(dt.getFullYear(), dt.getMonth(), dt.getDate() + 3).getDay()].slice(0, 3);
+// console.log(dayNext3);
+
+let dayNext4 = days[new Date(dt.getFullYear(), dt.getMonth(), dt.getDate() + 4).getDay()].slice(0, 3);
+// console.log(dayNext4);
+
+
 
 day1.innerHTML = dayNext1;
 day2.innerHTML = dayNext2;
@@ -61,7 +72,7 @@ day4.innerHTML = dayNext4;
 
 // let dayNextA = dayNext1.slice(0,3);
 
-// console.log(dayNext1,dayNext2,dayNext3,dayNext4);
+console.log(dayNext1, dayNext2, dayNext3, dayNext4);
 
 
 
@@ -83,19 +94,34 @@ function checkWeather(city) {
 
 
         if (data.cod === `404`) {
-            dayInfo.innerHTML = "Sorry' Location Not Found";
-            dayInfo.style = "color:red;font-size:2rem;height:123px";
+            content.forEach(i=>i.style.display="none")
+            document.querySelector(".error").style.display = "block";
             temperature.innerHTML = `0°C`;
             description.innerHTML = `Overcast Clouds`
             weather_img.src = "404.png";
-            list.style.opacity = "0";
+            list.style.display = "none";
             return;
         }
+        list.style.display = "flex";
+        document.querySelector(".error").style.display = "none";
+
+        
+        content.forEach(i=>i.style.display="flex")
+
 
         console.log(data);
 
 
-        
+        let time = new Date().getHours();
+        let minute = new Date().getMinutes();
+        if(time<10){
+            time = "0"+time;
+        }
+        if(minute<10){
+            minute = "0"+minute;
+        }
+        times.innerHTML = `${time}:${minute}`
+        // console.log(time, minute);
 
         temperature.innerHTML = `${Math.round(data.main.temp - 273.15)}°C`;
         description.innerHTML = `${data.weather[0].description}`;
@@ -145,25 +171,27 @@ function checkWeather(city) {
         const longitude = `${data.coord.lon}`;
         console.log(latitude);
         console.log(longitude);
-        function day_4 (lat,lon){
+        function day_4(lat, lon) {
 
-            const url2 = `https://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&appid=${api_key}`;
+            // const url2 = `https://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&appid=${api_key}`;
+
+            const url2 = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=d84e2ede8dfb4fa7ab2478082360658d&units=I&days=5`;
             fetch(url2).then(k => k.json()).then(data2 => {
                 console.table(data2);
 
 
-                day_temp1.innerHTML = `${Math.round(data2.list[1].main.temp - 273.15)}°C`;
-                day_temp2.innerHTML = `${Math.round(data2.list[9].main.temp - 273.15)}°C`;
-                day_temp3.innerHTML = `${Math.round(data2.list[17].main.temp - 273.15)}°C`;
-                day_temp4.innerHTML = `${Math.round(data2.list[25].main.temp - 273.15)}°C`;
-                switch (data2.list[1].weather[0].main) {
-                    case "Clouds":
+                day_temp1.innerHTML = `${Math.round((data2.data[1].temp - 32) * 5 / 9)}°C`;
+                day_temp2.innerHTML = `${Math.round((data2.data[2].temp - 32) * 5 / 9)}°C`;
+                day_temp3.innerHTML = `${Math.round((data2.data[3].temp - 32) * 5 / 9)}°C`;
+                day_temp4.innerHTML = `${Math.round((data2.data[4].temp - 32) * 5 / 9)}°C`;
+                switch (data2.data[1].weather.description) {
+                    case "Overcast clouds": case "Few clouds": case "Scattered clouds": case "Broken clouds":
                         day_img1.src = "cloud.png";
                         break;
-                    case "Clear":
+                    case "Clear Sky":
                         day_img1.src = "clear.png";
                         break;
-                    case "Rain":
+                    case "Moderate rain": case "Heavy rain":
                         day_img1.src = "rain.png";
                         break;
                     case "Mist":
@@ -183,16 +211,16 @@ function checkWeather(city) {
                         break;
                     default:
                         day_img1.src = "";
-        
+
                 }
-                switch (data2.list[9].weather[0].main) {
-                    case "Clouds":
+                switch (data2.data[2].weather.description) {
+                    case "Overcast clouds": case "Few clouds": case "Scattered clouds": case "Broken clouds":
                         day_img2.src = "cloud.png";
                         break;
-                    case "Clear":
+                    case "Clear Sky":
                         day_img2.src = "clear.png";
                         break;
-                    case "Rain":
+                    case "Moderate rain": case "Heavy rain":
                         day_img2.src = "rain.png";
                         break;
                     case "Mist":
@@ -212,16 +240,16 @@ function checkWeather(city) {
                         break;
                     default:
                         day_img2.src = "";
-        
+
                 }
-                switch (data2.list[17].weather[0].main) {
-                    case "Clouds":
+                switch (data2.data[3].weather.description) {
+                    case "Overcast clouds": case "Few clouds": case "Scattered clouds": case "Broken clouds":
                         day_img3.src = "cloud.png";
                         break;
-                    case "Clear":
+                    case "Clear Sky":
                         day_img3.src = "clear.png";
                         break;
-                    case "Rain":
+                    case "Moderate rain": case "Heavy rain":
                         day_img3.src = "rain.png";
                         break;
                     case "Mist":
@@ -241,16 +269,16 @@ function checkWeather(city) {
                         break;
                     default:
                         day_img3.src = "";
-        
+
                 }
-                switch (data2.list[25].weather[0].main) {
-                    case "Clouds":
+                switch (data2.data[4].weather.description) {
+                    case "Overcast clouds": case "Few clouds": case "Scattered clouds": case "Broken clouds":
                         day_img4.src = "cloud.png";
                         break;
-                    case "Clear":
+                    case "Clear Sky":
                         day_img4.src = "clear.png";
                         break;
-                    case "Rain":
+                    case "Moderate rain": case "Heavy rain":
                         day_img4.src = "rain.png";
                         break;
                     case "Mist":
@@ -270,12 +298,12 @@ function checkWeather(city) {
                         break;
                     default:
                         day_img4.src = "";
-        
+
                 }
             })
         }
 
-        day_4(latitude,longitude);
+        day_4(latitude, longitude);
 
 
 
